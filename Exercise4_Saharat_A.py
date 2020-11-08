fab478b1502631602377b6f5a37ed49d54cda139
white0104 <74053029+white0104@users.noreply.github.com>
2020-11-08 18:37:26 +0700

    Add files via upload

---
+++ Exercise4_Saharat_A.py
@@ -0,0 +1,9 @@
+FE = 75.5
+GB = 80
+ICS = 70
+CP = 80
+print("---Your Score---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
