fb36940592b0a45267c6762138aef0d7d217b708
white0104 <74053029+white0104@users.noreply.github.com>
2020-11-14 18:06:34 +0700

    Add files via upload

---
+++ Lecture50_Saharat_A.py
@@ -0,0 +1,12 @@
+def PlusNumber (x,y) :
+    print(x+y)
+def MinusNuber (x,y) :
+    print(x-y)
+def multiNumber (x,y) :
+    print(x*y)
+def divideNuber (x,y) :
+    print(x/y)
+PlusNumber(5,10)
+MinusNuber(10,5)
+multiNumber(10,5)
+divideNuber(10,5)
